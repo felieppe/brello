2b151af5746b8627ff88698cbbedf0598d61ca03
Felipe Cabrera <me@felieppe.com>
2024-08-27 17:24:17 -0300

    feat(home): add dynamic loading of columns and tasks related to it

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,35 @@
 import styles from '../styles/Home.module.css'
 
+import { useState } from "react";
+
 import Header from "../components/header";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
+    /*
+    
+        Column Example: 
+        {
+            id: 1,
+            name: "Column 1"
+        }
+
+        Task Example:
+        {
+            id: 1,
+            title: "Task 1",
+            description: "",
+            asigned: "",
+            priority: 0,
+            state: "",
+            limit: ""
+        }
+
+    */
+    const [columns, setColumns] = useState([{id: 1, name: "Backlog"}, {id: 2, name: "To Do"}, {id: 3, name: "In Progress"}, {id: 4, name: "Blocked"}, {id: 5, name: "Done"}]); 
+    const [tasks, setTasks] = useState([]);
+
     return (
         <main>
             <Header />
@@ -15,70 +40,29 @@ function Home() {
                 </div>
 
                 <div className={styles.board__content}>
-                    <div className={styles.board__content__column}>
-                        <div className={styles.column__header}>
-                            Backlog
-                        </div>
 
-                        <div className={styles.column__content}></div>
+                    {columns.map((column) => {
+                        return (
+                            <div className={styles.board__content__column} key={column.id}>
+                                <div className={styles.column__header}>
+                                    {column.name}
+                                </div>
 
-                        <div className={styles.column__add}>
-                            <FontAwesomeIcon icon={faPlus} />
-                            <p>Add a task</p>
-                        </div>       
-                    </div>
+                                {tasks.filter((t) => t.state == column.id).map((task) => {
+                                    return (
+                                        <div className={styles.task} key={task.id}>
+                                            <p>{task.title}</p>
+                                        </div>
+                                    );
+                                })}
 
-                    <div className={styles.board__content__column}>
-                        <div className={styles.column__header}>
-                            To Do
-                        </div>
-
-                        <div className={styles.column__content}></div>
-
-                        <div className={styles.column__add}>
-                            <FontAwesomeIcon icon={faPlus} />
-                            <p>Add a task</p>
-                        </div>       
-                    </div>
-
-                    <div className={styles.board__content__column}>
-                        <div className={styles.column__header}>
-                            In Progress
-                        </div>
-
-                        <div className={styles.column__content}></div>
-
-                        <div className={styles.column__add}>
-                            <FontAwesomeIcon icon={faPlus} />
-                            <p>Add a task</p>
-                        </div>       
-                    </div>
-
-                    <div className={styles.board__content__column}>
-                        <div className={styles.column__header}>
-                            Blocked
-                        </div>
-
-                        <div className={styles.column__content}></div>
-
-                        <div className={styles.column__add}>
-                            <FontAwesomeIcon icon={faPlus} />
-                            <p>Add a task</p>
-                        </div>       
-                    </div>
-
-                    <div className={styles.board__content__column}>
-                        <div className={styles.column__header}>
-                            Done
-                        </div>
-
-                        <div className={styles.column__content}></div>
-
-                        <div className={styles.column__add}>
-                            <FontAwesomeIcon icon={faPlus} />
-                            <p>Add a task</p>
-                        </div>       
-                    </div>
+                                <div className={styles.column__add}>
+                                    <FontAwesomeIcon icon={faPlus} />
+                                    <p>Add a task</p>
+                                </div>       
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </main>
