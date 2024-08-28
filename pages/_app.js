f6dcb6ed152d46ef0875c44fbf65c09eaff03182
Felipe Cabrera <me@felieppe.com>
2024-08-27 23:12:42 -0300

    fix(_app): add missing HEAD tag elements

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import 'bulma/css/bulma.css'
 
+import Head from "next/head";
+
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; 
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet='UTF-8' />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <title>Brello</title>
+      </Head>
+      
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
