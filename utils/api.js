be1aed5755a84f65902c2b543d8c072d5e5d9d5a
Felipe Cabrera <me@felieppe.com>
2024-09-02 12:51:12 -0300

    fix: add missing versioning of the BASE_URL

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3005'
+const BASE_URL = 'http://localhost:3005/v1'
 
 async function fetchTasks(filters) {
     const endpoint = `${BASE_URL}/tasks`
@@ -8,7 +8,7 @@ async function fetchTasks(filters) {
     try {
         const response = await axios.get(endpoint, { params: filters })
         return response.data["data"];
-    } catch(err) { throw err.response.data["error"]}
+    } catch(err) { throw err.response.data["error"] }
 }
 
 async function fetchTaskById(id) {
